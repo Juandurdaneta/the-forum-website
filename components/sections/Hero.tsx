bba59e3c78fc61e44038e37cd5dc56dfe4c90f8d
Juandurdaneta <juandurdanet@gmail.com>
2025-12-30 21:15:52 -0400

    Update Hero component to increase maximum width for better layout responsiveness

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -20,7 +20,7 @@ export function Hero() {
       <div className="absolute inset-0 opacity-[0.03] roman-pattern" />
 
       <div className="container mx-auto px-4 md:px-6 lg:px-8 relative z-10 pt-32 pb-20">
-        <div className="max-w-4xl mx-auto text-center">
+        <div className="max-w-6xl mx-auto text-center">
           {/* Badge */}
           <motion.div
             initial={{ opacity: 0, y: 20 }}
